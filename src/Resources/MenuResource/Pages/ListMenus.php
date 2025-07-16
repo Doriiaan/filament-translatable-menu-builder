3c4046d6354a79d07badd17f5f68705d7f87e9a3
Dorian <alary.dorian81@gmail.com>
2025-07-16 18:32:10 +0000

    Add translation after creation

--- src/Resources/MenuResource/Pages/ListMenus.php
+++ src/Resources/MenuResource/Pages/ListMenus.php
@@ -18,7 +18,10 @@ class ListMenus extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->after(function ($model) {
+                    $model->translateOrNew();
+                }),
         ];
     }
 }
